cfc9fe8664ef548d2640e5476600ba5b3fe27be0
Ian Malone <i.malone@ucl.ac.uk>
2019-10-01 15:05:37 +0100

    Modify spline.py to use natural end-points for knots.

--- spline.py
+++ spline.py
@@ -2,15 +2,20 @@ from scipy.interpolate import splev
 import numpy.linalg as linalg
 import numpy as np
 
-
+points=100
+Nphi=4
 #xi = np.arange(0,1,0.1)
-xi= np.linspace(0,1,10)
+# xk=xi # Previously used Nphi = xi.shape-4 for exact point fitting
+
+xi= np.linspace(0,1,points, endpoint=True)
+
+xk = np.linspace(0,1,Nphi, endpoint=True)
+xk = np.concatenate(([xk[0]]*2,xk,[xk[-1]]*2))
+
+
 f = np.sin(xi * np.pi)
 
-
-Nphi = xi.shape[0] - 4
 B = np.zeros((xi.shape[0], Nphi))
-xk=xi
 for p in range(0,Nphi):
    xp = np.zeros(Nphi)
    xp[p]=1
@@ -27,50 +32,3 @@ import matplotlib.pyplot as plt
 plt.plot(xi,testf,"lightblue")
 plt.plot(xi,f,"orangered")
 plt.show(block=False)
-
-
-M=[4,5,6]
-
-d=[1,1,1]
-order=3
-nk = [ x + (order+1) for x in M ]
-
-k = [ np.linspace(0,thisd,thisk) for thisd, thisk in zip(d,nk) ]
-
-N=[20,20,20]
-loc=[ np.linspace(0,thisd,thisN) for thisd, thisN in zip(d,N) ]
-
-def BforData1D(knots,datapoints, order=3):
-  Nphi = len(knots) - (order+1)
-  B = np.zeros((len(datapoints), Nphi))
-  for p in range(0,Nphi):
-    xp = np.zeros(Nphi)
-    xp[p] = 1
-    B[:,p] = splev(datapoints, (knots, xp, order))
-  return B
-
-# Fastest index is last in numpy (the order you get with .flat),
-# so stick with this convention, but N.B. it doesn't match the
-# on-disc storage order for the image!
-
-def orderedProduct(Z,Y,X):
-  prod = [ z * y * x for z in Z for y in Y for x in X ]
-  prod = np.array(prod)
-  return prod
-  
-
-B3d = [ BforData1D(k[dim],loc[dim]) for dim in range(0,3) ]
-
-Bfull = np.zeros((np.prod(N), np.prod(M)))
-for Z in range(0,N[0]) :
-  BZ = B3d[0][Z,]
-  for Y in range(0,N[1]) :
-    BY = B3d[1][Y,]
-    for X in range(0,N[2]) :
-      BX = B3d[2][X,]
-      row=X + Y * N[2] + Z * N[2] * N[1]
-      Bfull[row,] = orderedProduct(BZ,BY,BX)
-
-b1=np.matmul(Bfull.transpose(),Bfull)
-b2 = linalg.inv(b1)
-b3 = np.matmul(b2, Bfull.transpose())
