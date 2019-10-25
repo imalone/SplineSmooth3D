05541f5c96fd230e9cbc41c05515cc58edc48edd
Ian Malone <i.malone@ucl.ac.uk>
2019-10-25 12:13:56 +0100

    splinesmooth-coef-3d.py still not working properly, but notes on timing per step added.

--- splinesmooth-coef-3d.py
+++ splinesmooth-coef-3d.py
@@ -53,6 +53,7 @@ indsZpattern = np.tile(range(0,q1),q1*q1)
 indsYpattern = np.tile(np.repeat(range(0,q1),q1),q1) * kntsArr[0][0]
 indsXpattern = np.repeat(range(0,q1),q1*q1) * kntsArr[0][0] * kntsArr[1][0]
 
+needAtA=True
 for Z in range(0,shape[0]) :
   print (Z)
   (cIndZ, cZ) = coefArr[0][Z]
@@ -60,37 +61,52 @@ for Z in range(0,shape[0]) :
     (cIndY, cY) = coefArr[1][Y]
     for X in range(0,shape[2]) :
       (cIndX, cX) = coefArr[2][X]
-      # striping the BX, BY, BZ tensor into
-      # the outer-product order. Doesn't matter
-      # what choice we make so long as consistent
-      # going to end up with (q+1) * (q+1) * (q+1)
-      # coefficients, corresponding to the support
-      # cube, they then need to go in at intervals,
-      # q+1 long runs.
+      # 5us
+      # Times include ~ 5us overhead for loop and print.
+
+      # striping the BX, BY, BZ tensor into the outer-product order.
+      # Doesn't matter what choice we make so long as consistent.
+      # Going to end up with (q+1) * (q+1) * (q+1) coefficients,
+      # corresponding to the support cube, they then need to go in
+      # at intervals, q+1 long runs.
+
+      # Using pre-assigned out=array gives only about 1us improvement.
+      # pre-assigning arrays and using (a,b,out=preassigned)
       coefs = orderedProduct_asOuter(cZ,cY,cZ)
       coefsx = np.multiply(coeffs, inimgdata[Z,Y,X])
+      # 15us
+
       indsZ = indsZpattern + cIndZ
       indsY = indsYpattern + cIndY * kntsArr[0][0]
       indsX = indsXpattern + cIndX * kntsArr[0][0] * kntsArr[1][0]
       tgtinds = indsZ + indsY + indsX
+      # 18us
+      
       Atx[tgtinds] += coefsx
-      ## About 20us per voxel to reach this point.
-      AtAadd = np.outer(coefs,coefs)
-      ## 25us, outer products is about 5us
+      # 20us
 
-      ## A number of different AtA adding strategies to try to improve speed
-      ## With ix_ indexed adding, got about 66us per voxel, addition step
-      ## with ix_ is about 42us.
-      #AtA[np.ix_(tgtinds,tgtinds)] += AtAadd
-      ## Flat indexed adding for AtA whole iteration about 44us per
-      ## voxel (reshape) or 48us per vox (.flat):
-      ## flatind calculation takes about 9us, addition about 11us.
-      flatinds = tgtinds.reshape((q13,1)) + totalPar * tgtinds.reshape((1,q13))
-      AtAflat[flatinds.reshape(q13**2)] += AtAadd.reshape((q13**2))
-      #AtAflat[flatinds.flat] += AtAadd.flat
-      #t_now = time.time()
-      #print(t_now-t_last)
-      #t_last=t_now
+      if needAtA:
+        ## Pre-assigning array makes little difference
+        AtAadd = np.outer(coefs,coefs)
+        # 25-26us
+
+        ## Different AtA adding strategies to try to improve speed,
+        ## ix_ indexed adding step takes about 42us.
+        #AtA[np.ix_(tgtinds,tgtinds)] += AtAadd
+        ## Linear indexed adding for AtA whole iteration takes about
+        ## 20us, 9us for flatind, 11us for addition. Using .reshape()
+        ## around 4us faster than using .flat()
+
+        ## Pre-assigning seemingly slower?
+        flatinds = tgtinds.reshape((q13,1)) + \
+          totalPar * tgtinds.reshape((1,q13))
+        # 32-33 us
+
+        AtAflat[flatinds.reshape(q13**2)] += AtAadd.reshape((q13**2))
+        # 45us
+      t_now = time.time()
+      print(t_now-t_last)
+      t_last=t_now
 t_end=time.time()
 print(t_end)
 print(t_end-t_start)
